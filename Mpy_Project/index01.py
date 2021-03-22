7c1cbc92521601f81cd555f16eead21d687602e9
MOHAMMED56s <81165865+MOHAMMED56s@users.noreply.github.com>
2021-03-22 17:03:58 +0400

    Add files via upload

---
+++ Mpy_Project/index01.py
@@ -0,0 +1,15 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Program to print the modulus of two numbers")\num1=25
+SyntaxError: unexpected character after line continuation character
+>>> 
+>>> 
+>>> 
+>>> print("program to print the modulus of two numbers")
+program to print the modulus of two numbers
+>>> num1=25
+>>> num2=3
+>>> result = num2%num1
+>>> print(result)
+3
+>>> 
